12f01cec0723dcf5d4b29d902eb44e7dfa2b08ce
Uzay-G <uzgirit@gmail.com>
2022-01-18 20:04:20 +0100

    improve use of PAT to connect

--- archivy_git/__init__.py
+++ archivy_git/__init__.py
@@ -9,6 +9,7 @@ from archivy.models import DataObj
 # https://gitpython.readthedocs.io/en/stable/reference.html#git.remote.PushInfo
 ERROR_CODES = [1024, 8, 32, 16]
 
+
 def check_errored(flags):
     # gitpython returns flags to be checked by doing an AND with specified codes
     return any([flags & error_code for error_code in ERROR_CODES])
@@ -36,8 +37,18 @@ def setup():
         repo.active_branch.rename(branch)
 
         while True:
-            remote_url = click.prompt("Enter the url of the remote you'd like to sync to. "
-                                      "Ex: https://github.com/archivy/archivy", type=str)
+            remote_url = click.prompt(
+                "Enter the url of the remote you'd like to sync to. "
+                "Ex: https://github.com/archivy/archivy",
+                type=str,
+            )
+            username = click.prompt("Enter your username", type=str)
+            password = click.prompt(
+                "Enter your personal access token", type=str, hide_input=True
+            )
+            remote_url = remote_url.replace(
+                "https://", f"https://{username}:{password}@"
+            )
             origin = repo.create_remote("origin", remote_url)
             if origin.exists():
                 break
@@ -45,6 +56,7 @@ def setup():
         origin.push(branch)
         origin.fetch()
         repo.active_branch.set_tracking_branch(getattr(origin.refs, branch))
+        click.echo("Successfully setup repository.")
 
 
 @git.command()
@@ -56,7 +68,9 @@ def push(paths):
         repo.git.add(all=True)
     else:
         with app.app_context():
-            prefixed_paths = [os.path.join(app.config["USER_DIR"], path) for path in paths]
+            prefixed_paths = [
+                os.path.join(app.config["USER_DIR"], path) for path in paths
+            ]
         repo.index.add(prefixed_paths)
     repo.index.commit("Sync local changes to remote git repo.")
     push_event = repo.remotes.origin.push()[0]
@@ -73,7 +87,8 @@ def pull():
     pull_event = repo.remotes.origin.pull()[0]
     if check_errored(pull_event.flags):
         click.echo(f"Error during pull. {pull_event.note}")
-    else: click.echo("Sucessfully pulled changes from remote!")
+    else:
+        click.echo("Sucessfully pulled changes from remote!")
 
 
 def sync_dataobj(dataobj: DataObj):
