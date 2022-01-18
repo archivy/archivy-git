a58806cc6a779f69310f5e07dacd4631a8946414
Uzay-G <uzgirit@gmail.com>
2022-01-18 20:05:37 +0100

    v0.1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', encoding='utf-8') as f:
 
 setup(
     name='archivy_git',
-    version='0.1.3',
+    version='0.1.4',
     author="Uzay-G",
     author_email="halcyon@disroot.org",
     description=(
